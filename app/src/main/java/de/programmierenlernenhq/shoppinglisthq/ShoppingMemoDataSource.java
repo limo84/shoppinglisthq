4f831b00d774e4e030e1fc16c28ce9f585fa9e9a
Lukas Hartwig <lukas.hartwig@gmx.net>
2018-08-03 00:22:44 +0200

    first commit

---
+++ app/src/main/java/de/programmierenlernenhq/shoppinglisthq/ShoppingMemoDataSource.java
@@ -0,0 +1,21 @@
+package de.programmierenlernenhq.shoppinglisthq;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+
+public class ShoppingMemoDataSource {
+
+    private static final String LOG_TAG = ShoppingMemoDataSource.class.getSimpleName();
+
+    private SQLiteDatabase database;
+    private ShoppingMemoDbHelper dbHelper;
+
+
+    public ShoppingMemoDataSource(Context context) {
+        Log.d(LOG_TAG, "Unsere DataSource erzeugt jetzt den dbHelper.");
+        dbHelper = new ShoppingMemoDbHelper(context);
+    }
+}
